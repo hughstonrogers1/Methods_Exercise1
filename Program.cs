7f656285d6464b6a79e0cf17b433ba6a58fd63f0
hughstonrogers1 <hughstonrogers1>
2021-02-18 19:53:47 -0500

    Methods Exercise 1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Today we shall ask you a short series of questions to make a story. Have fun!");
+
+            Console.WriteLine("What is a name for your main character?");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Where do you want your chacter to be from?");
+            string From = Console.ReadLine();
+
+            Console.WriteLine("What is your characters class?");
+            string Class = Console.ReadLine();
+
+            Console.WriteLine("What is your character's age?");
+            string Age = Console.ReadLine();
+
+            Console.WriteLine($"The character you have selected is of the {Class} class." +
+                $"{Name} is the name that you have chosen for your character." +
+                $"{Name} is {Age} years old and is from the region of {From}. ");
+
+            
+        }
+
+
+    }
+}
